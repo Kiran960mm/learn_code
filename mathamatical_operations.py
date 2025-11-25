fa9b973873888a4325de90e2881212ec9f6994e8
Kiran960mm <kiranmoholkar960@gmail.com>
2025-11-25 19:05:25 +0530

    Add files via upload

---
+++ mathamatical_operations.py
@@ -0,0 +1,10 @@
+num1=int(input("Enter First Number :"))
+num2=int(input("Enter Second Number :"))
+add=num1+num2
+sub=num1-num2
+mul=num1*num2
+div=num1/num2
+print("Addition :",add)
+print("Subtraction :",sub)
+print("Multiplication :",mul)
+print("Division :",div)
